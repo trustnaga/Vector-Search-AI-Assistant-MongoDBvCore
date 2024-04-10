91a9dafc3a42fbe3ced8ef41bafdae5aee4b3cc9
trustnaga <bushan.kancharla@gmail.com>
2024-04-10 17:07:02 -0400

    updated

--- Search/Options/Chat.cs
+++ Search/Options/Chat.cs
@@ -6,7 +6,7 @@ public record Chat
 {
     public required MongoDbService MongoDbService { get; set; }
 
-    public required StorageService OpenAiService { get; set; }
+    public required OpenAiService OpenAiService { get; set; }
 
     public required ILogger Logger { get; init; }
 }
